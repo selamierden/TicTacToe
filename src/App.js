2fe1f1063af75028b936648d211daf8492a3c7e8
selamierdn <erdenselami07@gmail.com>
2023-03-14 23:39:01 +0300

    sending

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  function checkWinner(board) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
+
+  function handleClick(index) {
+    if (winner || board[index]) return;
+    const newBoard = [...board];
+    newBoard[index] = player;
+    setBoard(newBoard);
+    setPlayer(player === 'X' ? 'O' : 'X');
+    setWinner(checkWinner(newBoard));
+  }
+
+  function renderSquare(index) {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  }
+
+  function resetGame() {
+    setBoard(Array(9).fill(null));
+    setPlayer('X');
+    setWinner(null);
+  }
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${player}`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <button onClick={resetGame}>Reset</button>
+      </div>
     </div>
   );
 }
